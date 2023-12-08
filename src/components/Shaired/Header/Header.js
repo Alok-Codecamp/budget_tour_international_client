25fc64e63b82096e1b5fc8ac18d0620e1429820c
Alok-Codecamp <alok61.bd@gmail.com>
2023-12-08 19:16:35 +0600

    working on top  menu

--- src/components/Shaired/Header/Header.js
+++ src/components/Shaired/Header/Header.js
@@ -2,29 +2,25 @@ import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import './Header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeadSideMask, faHeadset, faPhone, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faHeadSideMask, faHeadset, faPhone, faUser,faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import support from './support.png'
 const Header = () => {
     return (
         <div className='header-container'>
-            <div className='menu-bar'>
                 <div className='top-menu'>
-                    <div className='contact-care'>
-                        <p className='call-1'><FontAwesomeIcon icon={faPhone} className='fa-phone'
-                            size='sm' />IND Toll Free: +8801738472661</p>
-                        <p className='call-2'>
+                        <a className='call-1'><FontAwesomeIcon icon={faPhone} className='fa-phone'
+                            size='sm' />IND Toll Free: +8801738472661</a>
+                        <a className='call-2'>
                             <FontAwesomeIcon icon={faPhone} className='fa-phone'
-                                size='sm' />UK Toll Free: +8801303312974</p>
-                        <p className='care'>
+                                size='sm' />UK Toll Free: +8801303312974</a>
+                        <a className='care'>
                             <img className='support-logo' src={support}/>
-                           We Care</p>
-                    </div>
-                    <div className='login'>
+                           We Care</a>
+                           <a className='login'>
                     <FontAwesomeIcon icon={faUser} className='login-logo' />
-                        <Link className='login-button' to='/login'>login</Link>
-
-                    </div>
-                </div>
+                        <Link className='login-button' to='/login'>Login</Link>
+                        <FontAwesomeIcon className='caretDown' icon={faCaretDown} size="sm"  />
+                    </a>
             </div>
            <h1>hello </h1>
         </div>
