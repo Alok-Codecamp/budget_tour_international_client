131ba3ed747f64854d6c017d3ce27bee9d8d8033
Alok-Codecamp <alok61.bd@gmail.com>
2023-12-10 00:24:02 +0600

    top menu complete

--- src/components/Shaired/Header/Header.js
+++ src/components/Shaired/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import './Header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -6,6 +6,18 @@ import {faPhone, faUser,faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import support from './support.png'
 import user from '../../Login/user.png'
 const Header = () => {
+    let [loginClick, setLoginClick] = useState(false);
+    const HandleLoginClick =()=>{
+        if(loginClick===false){
+            setLoginClick(true)
+            console.log("show");
+        }
+        else{
+            setLoginClick(false);
+            console.log("hide");
+        }
+
+    }
     return (
         <div className='header-container'>
                 <div className='top-menu'>
@@ -17,26 +29,36 @@ const Header = () => {
                         <a className='care' href='#'>
                             <img className='support-logo' src={support}/>
                            We Care</a>
-                           <a className='login' href='#'>
+                           <a onClick={HandleLoginClick} className='login' href='#'>
                     <FontAwesomeIcon icon={faUser} className='login-logo' />
-                        <a className='login-button'>Login</a>
+                        <span className='login-button'>Login</span>
                         <FontAwesomeIcon className='caretDown' icon={faCaretDown} size="sm"  />
                     </a>
-                    <div className='login-dropdown'>
+                    <div className='login-dropdown' style={{visibility:loginClick?"visible":"hidden"}}>
                            <div className='user-details'>
-                           <img className='login-profile-pic' src={user} alt="profile" />
-                           <button className='main-login-button'>Login</button>
+                           <img className='login-profile-pic' src={user} alt="profile" /><br/>
+                           <button  className='main-login-button'>Login</button>
                            <div className='new-user'>
                             <span className='register'>New user?</span><a className='register-button' href='#'>register</a>
                             </div>
                             </div>
-                            <p><Link className='link-button' to="/">Manage Booking</Link></p>
-                            <p><Link className='link-button' to="/">Cancelation</Link></p>
-                            <p><Link className='link-button' to='/'>Profile</Link></p>
+                           <div className='user-controle'>
+                           <p><Link className='user-controle-link-button' to="/">Manage Booking</Link></p>
+                            <p><Link className='user-controle-link-button' to="/">Cancelation</Link></p>
+                            <p><Link className='user-controle-link-button' to='/'>Profile</Link></p>
+                           </div>
                           
                            
                     </div>
             </div>
+            <nav className='main-menu'>
+            <a href="#">India Tour Packages</a>
+            <a href="#">International Tour Packages</a>
+            <a href="#">Hotels</a>
+            <a href="#">Transport</a>
+            <a href="#">About</a>
+            <a href="#">Contact Us</a>
+            </nav>
            
         </div>
     );
